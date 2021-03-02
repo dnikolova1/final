2a78141ae85b1767bd23bc98cf2d920c2d48798f
dnikolova1 <nikolovadara@gmail.com>
2021-03-02 17:59:58 -0600

    firebase configuration

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+    apiKey: "AIzaSyDr7y5Xy0DekirsCU1i_d4QIrtnllgQKmE",
+    authDomain: "kiei-451-a5ba9.firebaseapp.com",
+    projectId: "kiei-451-a5ba9",
+    storageBucket: "kiei-451-a5ba9.appspot.com",
+    messagingSenderId: "296217260745",
+    appId: "1:296217260745:web:43effe96466ba33af75318"
+} // updated to Dara's firebase config
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
