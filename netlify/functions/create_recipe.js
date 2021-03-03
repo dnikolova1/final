e7d6af42bf3638a0ae83baae1b168f93ae065c24
dnikolova1 <nikolovadara@gmail.com>
2021-03-03 12:27:08 -0600

    create_recipe_backend

--- netlify/functions/create_recipe.js
+++ netlify/functions/create_recipe.js
@@ -2,4 +2,40 @@ let firebase = require('./firebase')
 
 exports.handler = async function(event) {
   let db = firebase.firestore()
+  let body = JSON.parse(event.body)
+  let userId = body.userId
+  let username = body.username
+  let recipename = body.recipename
+  let recipeUrl = body.recipeUrl
+  let imageUrl = body.imageUrl
+  let ingredients = body.ingredients
+  let instructions = body.instructions
+  let userRating = body.userRating
   
+  console.log(`user: ${userId}`)
+  console.log(`imageUrl: ${imageUrl}`)
+  console.log(`recipename: ${recipename}`)
+
+  let newRecipe = { 
+    userId: userId,
+    username: username, 
+    recipename: recipename,
+    recipeUrl: recipeUrl,
+    imageUrl: imageUrl, 
+    ingredients: ingredients,
+    instructions: instructions,
+    userRating: userRating,
+    created: firebase.firestore.FieldValue.serverTimestamp()
+  }
+
+  let docRef = await db.collection('recipes').add(newRecipe)
+  newRecipe.id = docRef.id
+  newRecipe.upvotes = 0
+  newRecipe.type = 'Rated'
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(newPost)
+  }
+
+}
