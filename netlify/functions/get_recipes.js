6638361c55f113290de6397525bd72955eb0f514
dnikolova1 <nikolovadara@gmail.com>
2021-03-03 13:21:49 -0600

    get_recipes backend

---
+++ netlify/functions/get_recipes.js
@@ -0,0 +1,57 @@
+// /.netlify/functions/get_posts
+let firebase = require('./firebase')
+
+exports.handler = async function(event) {
+  let db = firebase.firestore()                             // define a variable so we can use Firestore
+  let recipesData = []                                        // an empty Array
+  
+  let recipesQuery = await db.collection('recipes')             // posts from Firestore
+                           .orderBy('created')              // ordered by created
+                           .get()
+  let recipes = recipesQuery.docs                               // the post documents themselves
+  
+  // loop through the post documents
+  for (let i=0; i<recipes.length; i++) {
+    let recipeId = recipes[i].id                                // the ID for the given post
+    let recipeData = recipes[i].data()                          // the rest of the post data
+    let likesQuery = await db.collection('likes')           // likes from Firestore
+                             .where('postId', '==', postId) // for the given postId
+                             .get()
+    let commentsQuery = await db.collection('comments')     // likes from Firestore
+                             .where('postId', '==', postId) // for the given postId
+                             .get()
+    let commentsData = []                                   // an empty Array
+    let comments = commentsQuery.docs                       // the comments documents
+
+    // loop through the comment documents
+    for (let i=0; i<comments.length; i++) {
+      let comment = comments[i].data()                      // grab the comment data
+      commentsData.push({
+        username: comment.username,                         // the author of the comment
+        text: comment.text                                  // the comment text
+      })
+    }
+
+    // add a new Object of our own creation to the postsData Array
+    recipesData.push({
+      id: postId,                                           // the post ID
+      imageUrl: postData.imageUrl,                          // the image URL
+      username: postData.username,                          // the username
+      likes: likesQuery.size,                               // number of likes
+      comments: commentsData,                                // an Array of comments
+      userId: postData.userId,
+      recipename: postData.recipename,
+      recipeUrl: postData.recipeUrl,
+      imageUrl: postData.imageUrl, 
+      ingredients: postData.ingredients,
+      instructions: postData.instructions,
+      userRating: postData.userRating
+    })
+  }
+  
+  // return an Object in the format that a Netlify lambda function expects
+  return {
+    statusCode: 200,
+    body: JSON.stringify(recipesData)
+  }
+}
